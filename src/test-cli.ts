a8e0b9df86f15c91c313c2b9b507d1565c1b4dd9
Anders Brams <abra@ambolt.io>
2021-02-12 18:01:00 +0100

    Added callable test cli script

---
+++ src/test-cli.ts
@@ -0,0 +1,15 @@
+#!/usr/local/bin/ts-node
+
+import { CliCommand } from './cli-command'
+
+const cli = new CliCommand('test-cli')
+  .withDescription('A test CLI that we can run from the command line.')
+  .withOptions([
+    { name: ['-v', '--verbose'], defaultValue: true },
+    { name: ['-f', '--files'], args: [{ name: 'files', variadic: true }], defaultValue: [] }
+  ])
+  .withArguments([
+    { name: 'dir', required: true }
+  ])
+
+console.log(JSON.stringify(cli.parse(process.argv), null, 2))
